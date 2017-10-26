7528202eb7c28ac5ddd3ab41d12122b5ce125e55
Erwin Tratar <erwin.tratar@gebit.de>
2017-10-26 14:59:33 +0200

    Revert "GBLD-81: share repository cache between maven invocations"
    This reverts commit b763715064fa74384204286e7077b41e473e6df3.

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/ProjectRegistryManager.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/ProjectRegistryManager.java
@@ -361,8 +361,6 @@ public class ProjectRegistryManager {
       IProgressMonitor monitor) throws CoreException {
     Set<IFile> secondPhaseBacklog = new LinkedHashSet<IFile>();
 
-    maven.invalidateSharedRepositoryCache();
-
     final Map<IFile, Set<Capability>> originalCapabilities = new HashMap<IFile, Set<Capability>>();
     final Map<IFile, Set<RequiredCapability>> originalRequirements = new HashMap<IFile, Set<RequiredCapability>>();
 
@@ -467,7 +465,6 @@ public class ProjectRegistryManager {
         continue;
       }
 
-
       MavenProjectFacade newFacade = null;
       if(pom.isAccessible() && pom.getProject().hasNature(IMavenConstants.NATURE_ID)) {
         newFacade = newState.getProjectFacade(pom);
@@ -509,7 +506,6 @@ public class ProjectRegistryManager {
   void refreshPhase2(MutableProjectRegistry newState, DependencyResolutionContext context,
       Map<IFile, Set<Capability>> originalCapabilities, Map<IFile, Set<RequiredCapability>> originalRequirements,
       IFile pom, MavenProjectFacade newFacade, IProgressMonitor monitor) throws CoreException {
-
     Set<Capability> capabilities = null;
     Set<RequiredCapability> requirements = null;
     if(newFacade != null) {
@@ -924,7 +920,6 @@ public class ProjectRegistryManager {
    */
   void applyMutableProjectRegistry(MutableProjectRegistry newState, IProgressMonitor monitor) throws CoreException {
     // don't cache maven sessions
-
     for(MavenProjectFacade facade : newState.getProjects()) {
       MavenProject mavenProject = getMavenProject(facade);
       if(mavenProject != null) {
