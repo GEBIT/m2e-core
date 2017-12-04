01739e467a57932852c2817f25e0ab227fa32238
Erwin Tratar <erwin.tratar@gebit.de>
2017-12-04 14:21:39 +0100

    NotDefaultModelCache extended

--- m2e-maven-runtime/org.eclipse.m2e.maven.runtime/src/main/java/org/apache/maven/project/NotDefaultModelCache.java
+++ m2e-maven-runtime/org.eclipse.m2e.maven.runtime/src/main/java/org/apache/maven/project/NotDefaultModelCache.java
@@ -19,26 +19,36 @@
 
 package org.apache.maven.project;
 
-import java.lang.reflect.Field;
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 import org.apache.maven.model.building.ModelCache;
 import org.eclipse.aether.RepositoryCache;
 import org.eclipse.aether.RepositorySystemSession;
 
 /**
- * Copy of org.apache.maven.repository.internal.DefaultModelCache, which is package private
+ * Implementation of {@link ModelCache} which shares keys with org.apache.maven.repository.internal.DefaultModelCache
+ * and caches the models in the {@link RepositoryCache} of the {@link RepositorySystemSession}.
  */
 public class NotDefaultModelCache
     implements ModelCache
 {
+    private static Class DefaultModelCacheKey_class;
+    private static Constructor DefaultModelCacheKey_new;
+
+    static {
+        try {
+            DefaultModelCacheKey_class = Class.forName("org.apache.maven.repository.internal.DefaultModelCache$Key");
+            DefaultModelCacheKey_new = DefaultModelCacheKey_class.getConstructor(String.class, String.class, String.class, String.class);
+            DefaultModelCacheKey_new.setAccessible(true);
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException exc) {
+            throw new RuntimeException("Failed to access DefaultModelCache.Key", exc);
+        }
+    }
 
     private final RepositorySystemSession session;
 
     private final RepositoryCache cache;
-    
-    private static final Map<Class, KeyAdapter> foreignKeyMap = new WeakHashMap<Class, KeyAdapter>();
 
     public NotDefaultModelCache( RepositorySystemSession session )
     {
@@ -48,138 +58,20 @@ public class NotDefaultModelCache
 
     public Object get( String groupId, String artifactId, String version, String tag )
     {
-        return cache.get( session, new Key( groupId, artifactId, version, tag ) );
+        return cache.get( session, createKey( groupId, artifactId, version, tag ) );
     }
 
     public void put( String groupId, String artifactId, String version, String tag, Object data )
     {
-        cache.put( session, new Key( groupId, artifactId, version, tag ), data );
+        cache.put( session, createKey( groupId, artifactId, version, tag ), data );
     }
 
-    static class Key
-    {
-
-        private final String groupId;
-
-        private final String artifactId;
-
-        private final String version;
-
-        private final String tag;
-
-        private final int hash;
-
-        public Key( String groupId, String artifactId, String version, String tag )
-        {
-            this.groupId = groupId;
-            this.artifactId = artifactId;
-            this.version = version;
-            this.tag = tag;
-
-            int h = 17;
-            h = h * 31 + this.groupId.hashCode();
-            h = h * 31 + this.artifactId.hashCode();
-            h = h * 31 + this.tag.hashCode();
-            hash = h;
-        }
-
-        @Override
-        public boolean equals( Object obj )
-        {
-            if ( this == obj )
-            {
-                return true;
-            }
-            if ( null == obj || !getClass().equals( obj.getClass() ) )
-            {
-                // try with reflection
-                KeyAdapter keyAdapter = null; 
-                try {
-                    if ( ! foreignKeyMap.containsKey( obj.getClass() ) ) {
-                        keyAdapter = new KeyAdapter( obj.getClass() );
-                        foreignKeyMap.put( obj.getClass(), keyAdapter );
-                    }
-                    keyAdapter = foreignKeyMap.get( obj.getClass() );
-                    if ( keyAdapter == null ) {
-                        return false;
-                    }
-                    return artifactId.equals( keyAdapter.getArtifactId( obj ) ) 
-                            && groupId.equals( keyAdapter.getGroupId( obj ) )
-                            && equalsVersion( version, keyAdapter.getVersion( obj ) ) 
-                            && tag.equals( keyAdapter.getTag( obj ) );
-                } catch (Exception exc) {
-                    // then it obviously is not possible
-                    foreignKeyMap.put( obj.getClass(), null );
-                }
-                return false;
-            }
-
-            Key that = (Key) obj;
-            return artifactId.equals( that.artifactId ) && groupId.equals( that.groupId )
-                && equalsVersion( version, that.version )
-                && tag.equals( that.tag );
-        }
-
-        @Override
-        public int hashCode()
-        {
-            return hash;
-        }
-
-        private boolean equalsVersion( String v1, String v2 ) {
-            if ( v1.equals( v2 ) ) {
-                return true;
-            }
-            int i1 = v1.indexOf( '$' );
-            int i2 = v2.indexOf( '$' );
-            if ( i1 >= 0 && i2 >= 0) {
-                return v2.substring( 0, i2 ).equals( v1.substring( 0, i1 ) );
-            } else if ( i1 >= 0 ) {
-                return v2.startsWith( v1.substring( 0, i1 ) );
-            } else if ( i2 >= 0 ) {
-                return v1.startsWith( v2.substring( 0, i2 ) );
-            } else {
-                return false;
-            }
-        }
-    }
-    static class KeyAdapter {
-        private Field groupIdField;
-
-        private Field artifactIdField;
-
-        private Field versionField;
-
-        private Field tagField;
-
-        KeyAdapter( Class keyClass ) throws NoSuchFieldException, SecurityException {
-            artifactIdField = keyClass.getDeclaredField("artifactId");
-            artifactIdField.setAccessible(true);
-
-            groupIdField = keyClass.getDeclaredField("groupId");
-            groupIdField.setAccessible(true);
-            
-            versionField = keyClass.getDeclaredField("version");
-            versionField.setAccessible(true);
-            
-            tagField = keyClass.getDeclaredField("tag");
-            tagField.setAccessible(true);
-        }
-
-        public String getArtifactId( Object key ) throws IllegalArgumentException, IllegalAccessException {
-            return (String) artifactIdField.get( key ); 
-        }
-        
-        public String getGroupId( Object key ) throws IllegalArgumentException, IllegalAccessException {
-        	return (String) groupIdField.get( key ); 
-        }
-        
-        public String getVersion( Object key ) throws IllegalArgumentException, IllegalAccessException {
-        	return (String) versionField.get( key ); 
-        }
-        
-        public String getTag( Object key ) throws IllegalArgumentException, IllegalAccessException {
-        	return (String) tagField.get( key ); 
-        }
+    static Object createKey( String groupId, String artifactId, String version, String tag ) {
+        try {
+            return DefaultModelCacheKey_new.newInstance(groupId, artifactId, version, tag);
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException exc) {
+            throw new RuntimeException("Failed to create DefaultModelCache.Key", exc);
+		}
     }
 }
