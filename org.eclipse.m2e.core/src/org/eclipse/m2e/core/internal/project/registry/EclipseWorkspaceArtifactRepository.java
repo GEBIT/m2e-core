7ce593049ea9bc74ce6dbda376b9f34a66c905c0
Erwin Tratar <erwin.tratar@gebit.de>
2017-12-12 14:11:50 +0100

    Optimized import and refresh

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/EclipseWorkspaceArtifactRepository.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/EclipseWorkspaceArtifactRepository.java
@@ -135,7 +135,9 @@ public final class EclipseWorkspaceArtifactRepository extends LocalArtifactRepos
         return null;
       }
 
-      return file.toFile();
+      if(file != null) {
+        return file.toFile();
+      }
     }
 
     return null;
