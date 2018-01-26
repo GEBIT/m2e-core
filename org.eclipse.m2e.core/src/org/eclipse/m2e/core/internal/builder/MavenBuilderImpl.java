cef398b8fc3f6c4960a5ab52e0cfdef680192c46
Erwin Tratar <erwin.tratar@gebit.de>
2018-01-26 16:50:59 +0100

    fix case where project itself is refreshed

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/builder/MavenBuilderImpl.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/builder/MavenBuilderImpl.java
@@ -247,6 +247,8 @@ public class MavenBuilderImpl {
           IResource resource;
           if(!file.exists()) {
             resource = project.findMember(path);
+          } else if(path.isEmpty()) {
+            resource = project;
           } else if(file.isDirectory()) {
             resource = project.getFolder(path);
           } else {
@@ -285,6 +287,10 @@ public class MavenBuilderImpl {
       return null;
     }
 
+    if(filePath.equals(projectPath)) {
+      // project itself
+      return new Path("");
+    }
     return filePath.removeFirstSegments(projectPath.segmentCount());
   }
 
