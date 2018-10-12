48831ea303d95814ed1870b6fedd596d8a8fec9b
Carsten Pfeiffer <carsten.pfeiffer@gebit.de>
2018-10-12 14:06:01 +0200

    GBLD-473: optimization: remove markers only on builders with markers, javadoc + delta fix

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/BuildPreventer.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/internal/BuildPreventer.java
@@ -27,25 +27,53 @@ import org.eclipse.jdt.core.JavaCore;
 
 
 /**
+ * <p>
  * This is a builder that shall be placed in front of the Java builder in the .project configuration file. It will check
  * if the project has the java nature and java builder. If both are there, but the .classpath file is missing, the
  * builder will add an error marker to the project and cancel the build. This prevents the java builder from building
  * the project into the default bin/-folder and thus preventing other tools like integrity or compasscript from parsing
  * files in the bin-folder (which is not configured to be an output folder, so cannot be ignored).
+ * </p>
  *
  * <pre>
  * BIG NOTE:
  * Unfortunately this has the side effect that not only the currently building project will be aborted, but the entire
  * workspace build! Use the de.gebit.maven.m2e plugin to visualize such cases.
  * </pre>
+ *
+ * To make this visible to the user, all other projects that were prevented from building get an additional "folloup"
+ * marker that points to the project causing this.
+ * <p>
+ * Implementation-wise this is a little tricky: A new instance of this class will be created for every project that is
+ * configured with this builder. If any such instance will prevent the build because of a missing .classpath file, it
+ * will look up <b>all</b> the followup markers <b>of all other projects</b> in the workspace, delete those that are not
+ * needed anymore, update those that need updating and create any missing ones.
+ * </p>
+ * <p>
+ * When no instance will abort the build, <b>all "affected" instances</b> will look up and delete all such markers <b>of
+ * all projects</b>. "Affected" means, any instance whose project has such a marker itself. The reason for this is that
+ * there is no sensible way to share information about a whole workspace-build across instances. However, as soon as the
+ * first instance removed all markers, subsequent instances should not do much, simply because there are no more
+ * markers. Additionally, no instance can rely on another one doing the job or being availale at all.
+ * </p>
  */
 public class BuildPreventer extends IncrementalProjectBuilder {
 
+  /**
+   * The ID of this builder.
+   */
   public static final String BUILDER_ID = "org.eclipse.m2e.jdt.buildpreventer";
 
+  /**
+   * The ID of the marker causing the entire workspace build to abort, due to missing .classpath file.
+   */
   public static final String ERROR_MARKER_ID = "org.eclipse.m2e.jdt.classpathmissing";
 
-  private static final String FOLLOWUP_ERROR_MARKER_ID = "org.eclipse.m2e.jdt.buildprevented_followup";
+  /**
+   * The ID of the marker added on all projects that were not built because an {@link #ERROR_MARKER_ID} caused the build
+   * to be aborted.
+   */
+  public static final String FOLLOWUP_ERROR_MARKER_ID = "org.eclipse.m2e.jdt.buildprevented_followup";
 
   public BuildPreventer() {
   }
@@ -69,8 +97,12 @@ public class BuildPreventer extends IncrementalProjectBuilder {
         forgetLastBuiltState();
         return new IProject[] {tempProject};
       }
-      removeAllMarkers(tempOldErrorMarkers);
-      removeAllMarkers(tempOldFollowupMarkers);
+
+      // optimization: let only those builders remove markers that have an error marker or followup marker themselves
+      if(tempOldErrorMarkers.get(tempProject) != null || tempOldFollowupMarkers.get(tempProject) != null) {
+        removeAllMarkers(tempOldErrorMarkers);
+        removeAllMarkers(tempOldFollowupMarkers);
+      }
     } catch(CoreException ex) {
       // error accessing the project description, let the other builders continue
     }
@@ -78,19 +110,29 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * @return
+   * @return <code>true</code> if there are changes in the delta that make us need to check for build prevention
    */
   private boolean needsBuild() {
     final boolean[] tempResult = new boolean[1];
     IResourceDelta tempDelta = getDelta(getProject());
-    if(tempDelta == null) {
+    if(tempDelta == null) { // e.g. a "Clean" build => force building
       return true;
     }
     try {
       tempDelta.accept(new IResourceDeltaVisitor() {
 
         public boolean visit(IResourceDelta aDelta) throws CoreException {
-          if(aDelta.getFlags() != IResourceDelta.MARKERS) {
+          int tempKind = aDelta.getKind();
+          if((tempKind & (IResourceDelta.ADDED | IResourceDelta.REMOVED)) != 0) {
+            tempResult[0] = true;
+            return false;
+          }
+
+          int tempFlags = aDelta.getFlags();
+          if(tempFlags == 0) {
+            return true;
+          }
+          if(tempFlags != IResourceDelta.MARKERS) {
             tempResult[0] = true;
             return false;
           }
@@ -98,7 +140,6 @@ public class BuildPreventer extends IncrementalProjectBuilder {
         }
       });
     } catch(CoreException ex) {
-      // TODO Auto-generated catch block
       log("Error analyzing build delta.", ex);
     }
 
@@ -106,9 +147,15 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * @param errorMarkerId
+   * Removes all the given markers.
+   *
+   * @param someMarkers the markers to remove
    */
   private void removeAllMarkers(Map<IProject, IMarker> someMarkers) {
+    if(someMarkers.isEmpty()) { // to simplify debugging
+      return;
+    }
+
     for(IMarker tempMarker : someMarkers.values()) {
       try {
         tempMarker.delete();
@@ -119,8 +166,9 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * @param errorMarkerId
-   * @return
+   * Locates all markers of the given ID on all projects in the workspace
+   *
+   * @param aMarkerId the ID of the markers to locate
    */
   private Map<IProject, IMarker> findAllMarkers(String aMarkerId) {
     try {
@@ -140,15 +188,21 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * Adds an error marker to the project and cancels the build.
+   * This method does the following:
+   * <ol>
+   * <li>it immediately cancels the progress monitor, so that no further concurrent builders may be invoked</li>
+   * <li>it adds an {@link #ERROR_MARKER_ID error marker} to the given project (if not available yet)</li>
+   * <li>it removes all other existing {@link #ERROR_MARKER_ID error markers} on other projects</li>
+   * <li>it ensures that followup markers will exist on exactly those projects that are not built during this cycle</li>
+   * </ol>
    *
-   * @param aProject
-   * @param tempOldErrorMarkers
-   * @param someOldFollowupMarkers
-   * @param aMonitor
+   * @param aProject the project this builder runs on
+   * @param someOldErrorMarkers all error markers existing prior to this build
+   * @param someOldFollowupMarkers all followup markers existing prior to this build
+   * @param aMonitor the monitor to cancel, causing the entire workspace build to be canceled
    * @throws CoreException
    */
-  private void preventBuild(IProject aProject, Map<IProject, IMarker> tempOldErrorMarkers,
+  private void preventBuild(IProject aProject, Map<IProject, IMarker> someOldErrorMarkers,
       Map<IProject, IMarker> someOldFollowupMarkers, IProgressMonitor aMonitor) {
 
     aMonitor.setCanceled(true);
@@ -156,7 +210,7 @@ public class BuildPreventer extends IncrementalProjectBuilder {
     // first, remove any old *error* marker (usually just one)
     try {
       boolean tempAddNewErrorMarker = true;
-      for(IMarker tempMarker : tempOldErrorMarkers.values()) {
+      for(IMarker tempMarker : someOldErrorMarkers.values()) {
         if(aProject.equals(tempMarker.getResource())) {
           tempAddNewErrorMarker = false;
         } else {
@@ -175,7 +229,13 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * Creates followup markers on all projects that have not been built. If the projects already have a marker, it
+   * Ensures that followup markers
+   * <ul>
+   * <li>are updated on all projects where they are not up to date</li>
+   * <li>are created on all projects that need, but lack one</li>
+   * <li>are removed on all projects not needing one anymore</li>
+   * </ul>
+   * on all projects that have not been built. If the projects already have a marker, it
    *
    * @param anAbortedProject
    * @param someOldFollowupMarkers
@@ -266,9 +326,10 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * Adds a persistent syntax error marker to the given project.
+   * Adds a persistent error marker to the given project.
    *
-   * @param aFile the file to add the marker to.
+   * @param aProject the project to add the marker to.
+   * @see #ERROR_MARKER_ID
    * @throws CoreException
    */
   public void addOriginErrorTo(IProject aProject) throws CoreException {
@@ -278,23 +339,32 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * Adds a persistent syntax error marker to the given file.
+   * Adds a persistent followup marker to the given file.
    *
-   * @param someOldFollowupMarkers
-   * @param aFile the file to add the marker to.
+   * @param aProject the project to add the marker to.
+   * @param anAbortedProject the project that caused the build to be aborted
+   * @see #FOLLOWUP_ERROR_MARKER_ID
    * @throws CoreException
    */
   public void addFollowupErrorTo(IProject aProject, IProject anAbortedProject) throws CoreException {
-    addErrorTo(aProject, FOLLOWUP_ERROR_MARKER_ID,
-        "Project was not built because of project " + anAbortedProject.getName(),
+    addErrorTo(aProject, FOLLOWUP_ERROR_MARKER_ID, getFollowupMarkerMessageFor(anAbortedProject),
         getMarkerLocationValueFor(anAbortedProject), IMarker.PRIORITY_HIGH);
   }
 
   /**
-   * Adds a persistent syntax error marker to the given file.
+   * Returns the message for a followup marker, pointing to the project that aborted the build.
+   *
+   * @param anAbortedProject the project that caused the build to be aborted
+   */
+  private String getFollowupMarkerMessageFor(IProject anAbortedProject) {
+    return "Project was not built because of project " + anAbortedProject.getName();
+  }
+
+  /**
+   * Adds a persistent marker to the given file.
    *
    * @param aLocation
-   * @param aFile the file to add the marker to.
+   * @param aProject the project to add the marker to.
    * @throws CoreException
    */
   public void addErrorTo(IProject aProject, String aMarkerId, String aMessage, String aLocation, int aPriority)
@@ -302,21 +372,16 @@ public class BuildPreventer extends IncrementalProjectBuilder {
     if(!aProject.isAccessible()) {
       return;
     }
-
-//    IMarker[] tempMarkers = aProject.findMarkers(aMarkerId, false, IResource.DEPTH_ZERO);
-//    if(tempMarkers != null && tempMarkers.length > 0) {
-//      return; // no need to add another one
-//    }
     createMarkerFor(aProject, aMarkerId, aMessage, aLocation, aPriority);
   }
 
   /**
-   * Creates an error marker for the given file.
+   * Creates an error marker for the given project.
    *
+   * @param aProject
    * @param aPriority
    * @param aMessage
    * @param aMarkerId
-   * @param aFile
    * @return
    * @throws CoreException
    */
@@ -333,15 +398,21 @@ public class BuildPreventer extends IncrementalProjectBuilder {
   }
 
   /**
-   * @param tempBadExistingMarker
-   * @param tempProject
+   * Updates the attributes of an existing marker.
+   * 
+   * @param aBadExistingMarker
+   * @param aNewAbortedProject
    * @throws CoreException
    */
   private void updateFollowupError(IMarker aBadExistingMarker, IProject aNewAbortedProject) throws CoreException {
-    aBadExistingMarker.setAttribute(IMarker.LOCATION, getMarkerLocationValueFor(aNewAbortedProject));
+    aBadExistingMarker.setAttributes(new String[] {IMarker.MESSAGE, IMarker.LOCATION},
+        new Object[] {getFollowupMarkerMessageFor(aNewAbortedProject), getMarkerLocationValueFor(aNewAbortedProject)});
   }
 
   /**
+   * Checks whether the given marker is already up to date regarding the project that aborted the build. Returns
+   * <code>false</code> if the project needs to be {@link #updateFollowupError(IMarker, IProject) updated}
+   * 
    * @param aMarker the marker to check, or <code>null</code> if it does not exist
    * @param anAbortedProject
    * @return
@@ -357,6 +428,11 @@ public class BuildPreventer extends IncrementalProjectBuilder {
     return false;
   }
 
+  /**
+   * Returns the value for the given project to be set for the IMarker.LOCATION attribute.
+   * 
+   * @param anAbortedProject
+   */
   private String getMarkerLocationValueFor(IProject anAbortedProject) {
     return anAbortedProject.getName();
   }
@@ -380,23 +456,5 @@ public class BuildPreventer extends IncrementalProjectBuilder {
       return tempResult;
     }
     return Collections.emptyList();
-  }
-
-  private void removeMarker(IProject aProject, String aMarkerId) {
-    if(!aProject.isAccessible()) {
-      return;
-    }
-
-    try {
-      IMarker[] tempMarkers = aProject.findMarkers(aMarkerId, false, IResource.DEPTH_ZERO);
-      if(tempMarkers == null || tempMarkers.length == 0) {
-        return; // nothing to remove
-      }
-      for(IMarker tempMarker : tempMarkers) {
-        tempMarker.delete();
-      }
-    } catch(CoreException ex) {
-      log("Cannot remove error marker from: " + aProject.getName(), ex);
-    }
   }
 }
