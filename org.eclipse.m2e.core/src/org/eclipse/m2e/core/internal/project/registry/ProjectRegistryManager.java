00647fb515ecef68cb523f220246230666dd5d1d
Erwin Tratar <erwin.tratar@gebit.de>
2019-08-06 14:00:10 +0200

    don't process twice

--- org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/ProjectRegistryManager.java
+++ org.eclipse.m2e.core/src/org/eclipse/m2e/core/internal/project/registry/ProjectRegistryManager.java
@@ -492,6 +492,7 @@ public class ProjectRegistryManager {
     context.forcePomFiles(allProcessedPoms);
 
     // phase 2: resolve project dependencies
+    Set<IFile> secondPhaseProcessed = new HashSet<IFile>();
     while(!context.isEmpty()) {
       if(monitor.isCanceled()) {
         throw new OperationCanceledException();
@@ -1126,7 +1127,12 @@ public class ProjectRegistryManager {
       }
     }
     if(mavenProject == null) {
-      mavenProject = getContextProjects().get(facade);
+      // look in context cache first to avoid project building
+      for(Map.Entry<MavenProjectFacade, MavenProject> entry : getContextProjects().entrySet()) {
+        if(entry.getKey().getArtifactKey().equals(facade.getArtifactKey())) {
+          return entry.getValue();
+        }
+      }
     }
     return mavenProject;
   }
